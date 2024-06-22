34b6984533302c97ba33b77b36324fb2ec350776
GolaJakub <kuba300901@gmail.com>
2024-06-22 04:53:20 +0200

    regex fixes

--- src/app/users/personal-data/personal-data.component.ts
+++ src/app/users/personal-data/personal-data.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {Router, RouterLink} from '@angular/router';
-import {NgForOf, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault} from "@angular/common";
-import {UserStateService} from "../user-state.service";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {UsersService} from "../users.service";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { NgForOf, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from "@angular/common";
+import { UserStateService } from "../user-state.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { UsersService } from "../users.service";
 
 @Component({
   selector: 'tm-personal-data',
@@ -25,20 +25,26 @@ export class PersonalDataComponent implements OnInit {
   menteeDetailsForm: FormGroup;
 
   fields = [
-    {name: 'email', label: 'Email'},
-    {name: 'firstname', label: 'First Name'},
-    {name: 'lastname', label: 'Last Name'},
-    {name: 'phone', label: 'Phone Number'},
-    {name: 'dateOfBirth', label: 'Date of Birth'},
-    {name: 'gender', label: 'Gender'},
-    {name: 'height', label: 'Height'},
+    { name: 'email', label: 'Email' },
+    { name: 'firstname', label: 'First Name' },
+    { name: 'lastname', label: 'Last Name' },
+    { name: 'phone', label: 'Phone Number' },
+    { name: 'dateOfBirth', label: 'Date of Birth' },
+    { name: 'gender', label: 'Gender' },
+    { name: 'height', label: 'Height' },
   ];
 
-  constructor(private fb: FormBuilder, private router: Router, private userStateService: UserStateService, private snackBar: MatSnackBar, private userService: UsersService) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private userStateService: UserStateService,
+    private snackBar: MatSnackBar,
+    private userService: UsersService
+  ) {
     this.menteeDetailsForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      firstname: ['', [Validators.required, Validators.pattern('^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+$')]],
-      lastname: ['', [Validators.required, Validators.pattern('^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+$')]],
+      firstname: ['', [Validators.required, Validators.pattern('^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ -]+$')]],
+      lastname: ['', [Validators.required, Validators.pattern('^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ -]+$')]],
       phone: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
       dateOfBirth: ['', Validators.required],
       gender: ['', Validators.required],
@@ -61,7 +67,7 @@ export class PersonalDataComponent implements OnInit {
     }
 
     if (currentUser?.firstLogin && currentUser?.role === "MENTEE") {
-      this.snackBar.open('It\'s your first login, you need to complete your personal info and initial report to continue.', 'OK', {duration: 10000});
+      this.snackBar.open('It\'s your first login, you need to complete your personal info and initial report to continue.', 'OK', { duration: 10000 });
     }
   }
 
@@ -76,18 +82,18 @@ export class PersonalDataComponent implements OnInit {
 
   save(): void {
     if (this.menteeDetailsForm.valid) {
-      const updatedMentee = {...this.menteeDetailsForm.value};
+      const updatedMentee = { ...this.menteeDetailsForm.value };
 
       this.userService.saveMentee(updatedMentee).subscribe({
         next: () => {
-          this.snackBar.open('Personal data saved successfully!', 'OK', {duration: 3000});
+          this.snackBar.open('Personal data saved successfully!', 'OK', { duration: 3000 });
           if (this.isMenteeFirstLogin()) {
             this.router.navigate(['/periodical-report']);
           }
           this.userStateService.loadCurrentUser();
         },
         error: (message) => {
-          this.snackBar.open(message.error[0].description, 'Close', {duration: 3000});
+          this.snackBar.open(message.error[0].description, 'Close', { duration: 3000 });
         }
       });
     } else {
@@ -98,5 +104,4 @@ export class PersonalDataComponent implements OnInit {
   onCancel(): void {
     window.history.back();
   }
-
 }
